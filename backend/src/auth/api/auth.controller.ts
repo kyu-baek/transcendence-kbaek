4b0b2e5b6a639b410e0565bf27505d39c9ecb53f
kyu-baek <rbfla6334@gmail.com>
2023-05-14 21:01:19 +0900

    UPDATE: kakao login guard

--- backend/src/auth/api/auth.controller.ts
+++ backend/src/auth/api/auth.controller.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, Get, Post, Query, Res, UnauthorizedException, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Header, Post, Query, Res, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../../models/user/entities';
 import { Repository } from 'typeorm';
 import { FtGuard } from '../../common/guards/ft/ft.guard';
 import { AuthService } from './services';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { TwoFactorGuard } from '../../common/guards/twoFactor/twoFactor.guard';
 import { GetGuardData } from '../../common/decorators';
 import { JwtGuard } from '../../common/guards/jwt/jwt.guard';
@@ -13,7 +13,16 @@ import { FtDataInterface } from '../../common/interfaces/FtData.interface';
 import { JwtPayloadInterface } from '../../common/interfaces/JwtUser.interface';
 import { EmailReqDto } from '../../models/user/api/dtos/verifyEmailReq.dto';
 import { VerifyEmailToken } from '../../models/user/api/dtos/verifyEmailToken.dto';
+import { KakaoGuard } from 'src/common/guards/kakao/kakao.guard';
 
+
+interface IOAuthUser {
+  user: {
+    name: string;
+    email: string;
+    password: string;
+  };
+}
 
 @Controller('auth')
 export class AuthController {
@@ -61,6 +70,9 @@ export class AuthController {
   }
 
 
+  /* 
+    email login
+  */
   @Post('/signin/email')
   async verifyEmail(
     @Body() payload: EmailReqDto,
@@ -78,7 +90,67 @@ export class AuthController {
     return await this.authService.confirmEmailToken(dto, res);
   }
 
+  /* 카카오 로그인 */
 
+  @Get('kakaoLogin')
+  @Header('Content-Type', 'text/html')
+  getKakaoLoginPage(): string {
+    return `
+      <div>
+        <h1>카카오 로그인</h1>
+        <form action="/kakaoLoginLogic" method="GET">
+          <input type="submit" value="카카오로그인" />
+        </form>
+        <form action="/kakaoLogout" method="GET">
+          <input type="submit" value="카카오로그아웃 및 연결 끊기" />
+        </form>
+      </div>
+    `;
+  }
+
+
+  
+  
+  @UseGuards(KakaoGuard)
+  @Get("/redirect/kakao")
+  async loginKakao(
+    @GetGuardData() data: FtDataInterface,
+    @Res() res: Response
+  ) {
+    // this.authService.OAuthLogin({ req, res });
+
+    try {
+      return await this.authService.redirect(data, res);
+    } catch (e) {
+      if (e instanceof UnauthorizedException) {
+        res.redirect('/signin_duplicated');
+      } else {
+        res.redirect('/signin_fail');
+      }
+    }
+  }
+
+  // /* 구글 로그인 */
+  // @UseGuards(AuthGuard("google"))
+  // @Get("/signin/google")
+  // async loginGoogle(
+  //   @Req() req: Request & IOAuthUser, //
+  //   @Res() res: Response
+  // ) {
+  //   this.authService.OAuthLogin({ req, res });
+  // }
+
+  // /* 네이버 로그인 */
+  // @UseGuards(AuthGuard("naver"))
+  // @Get("/signin/naver")
+  // async loginNaver(
+  //   @Req() req: Request & IOAuthUser, //
+  //   @Res() res: Response
+  // ) {
+  //   this.authService.OAuthLogin({ req, res });
+  // }
   
 
 }
+
+
